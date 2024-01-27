a84b40115d68d9ea3076d393c80f3956c545f300
Nelson Parajuli <nelsonparajuli2064@gmail.com>
2024-01-28 05:27:27 +0545

    largest string on the basis of lexicographpy

--- String/cone.java
+++ String/cone.java
@@ -2,6 +2,14 @@ package String;
 
 public class cone {
     public static void main(String[] args){
-        
+        String list[] = {"apple" , "mango" , "banana"};
+        //print the largest string
+        String max = list[0];
+        for(int i = 1 ; i<list.length; i++){
+            if(list[i].compareToIgnoreCase(max) > 0){
+                max = list[i];
+            }
+        }
+        System.out.println(max);
     }
 }
---
+++ String/done.java
@@ -0,0 +1,8 @@
+package String;
+
+public class done {
+    public static void main(String[] args){
+     
+    }
+    
+}
